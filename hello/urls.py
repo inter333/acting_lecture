f995ef0e4cb177a814be19a9b625ca1f38be2a95
中山健太 <kenta333@nakayamakentanoMacBook-Pro.local>
2019-03-15 19:03:28 +0900

    詳細ページまで

--- hello/urls.py
+++ hello/urls.py
@@ -1,13 +1,6 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Feb  9 00:00:25 2019
-
-@author: namek
-"""
-
 from django.conf.urls import url
-from . views import HelloView
+from .views import HelloView
 
-urlpatterns = [
-        url(r'',HelloView.as_view(),name='index'),
+urlpatterns =[
+        url(r'', HelloView.as_view(),name='index'),
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,30 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Friend
+from django.views.generic import TemplateView
+from .forms import HelloForm
 
 class HelloView(TemplateView):
-    
+
     def __init__(self):
-        self.params={
-                'title':'Hello',
-                'form':HelloForm(),
-                'result':None
+        self.params = {
+               'title':'代行要請',
+               'message':'コマの情報',
+               'form':HelloForm()
             }
-        
+    
     def get(self,request):
         return render(request,'hello/index.html',self.params)
-    
+
+
     def post(self,request):
-        ch=request.POST.getlist('choice')
-        result='<ol><b>selected:</b>'
-        for item in ch:
-            result +='<li>'+item+'</li>'
-        result+='</ol>'
-        self.params['result']=result
-        self.params['form']=HelloForm(request.POST)
+        msg = '入力した情報は以下の通りですか？<br><b>'  '(' + request.POST['date'] + \
+            ')' '(' + request.POST['time'] + \
+            ')</b><b>' + request.POST['name'] + \
+            '(' + request.POST['grade'] + \
+            ')' + request.POST['subject'] + \
+            '(' + request.POST['remark'] + \
+            ')' '</b>'
+        self.params['message'] = msg
+        self.params['form'] = HelloForm(request.POST)
+        self.params['message'] = msg
+        self.params['form'] = HelloForm(request.POST)
         return render(request,'hello/index.html',self.params)
 
 
 
-
-# Create your views here.
